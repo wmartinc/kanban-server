e4ef9aebd61e3cc2fd5ff3c13a8d27dce26e4f9b
Martin <coxmartin877@gmail.com>
2026-06-20 23:15:46 -0600

    Se modifico el archivo de jwt para que la informacion del board seleccionado no se almacene en la cookie, y en login se modifico el endpoint de login para que se obtenga de la base de datos informacion mas reciente, en databaseActions/user se le agrego otro metodo para poder obtener informacion del usuario a traves del id

--- databaseActions/user.js
+++ databaseActions/user.js
@@ -11,6 +11,23 @@ const getUsuarioByEmail = async (email) => {
   }
 }
 
+const getUserById = async (userId) => {
+  try {
+    const { data } = await conexion
+      .from('user')
+      .select('*')
+      .eq('id', userId);
+    
+    console.log("Datos desde data: ", data)
+    if(data.length > 0) return data[0]
+    return false
+  } catch (error) {
+    console.log(error.message)
+    return false
+  }
+}
+
 module.exports = {
-  getUsuarioByEmail
+  getUsuarioByEmail,
+  getUserById
 }
--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const loginRoute = express.Router();
-const { getUsuarioByEmail } = require('../databaseActions/user')
+const { getUsuarioByEmail, getUserById } = require('../databaseActions/user')
 const { createSesion, verifySesion } = require('../utils/jwtVerifications');
 const { verifyPassword } = require('../utils/verifications');
 
@@ -16,16 +16,19 @@ loginRoute.post('/login', async (req, res) => {
   const sesionCreated = createSesion(res, usuario);
   if (!sesionCreated) return res.status(500).json({ confirmation: false, message: 'Something went wrong!' });
   const {id, password:_, created_at, ...newUser} = usuario
-
   return res.status(200).json({ confirmation: true, newUser })
 })
 
-loginRoute.get('/login', verifySesion, (req, resp) => {
-  if(req.user) {
-    console.log(req.user)
-    return resp.status(200).json({ confirmation: true, content: req.user })
-  }
-  return resp.status(500).json({ confirmation: false, content: 'Something went wrong!' });
+loginRoute.get('/login', verifySesion, async (req, resp) => {
+  const savedUser = req.user
+  if(!savedUser)  return resp.status(500).json({ confirmation: false, content: 'Something went wrong!' });
+
+  const dbUser = await getUserById(savedUser.id)
+  if(!dbUser) return resp.status(500).json({confirmation: false, content: 'Something went wrong!'})
+  savedUser.main_board = dbUser.main_board
+
+  return resp.status(200).json({ confirmation: true, content: savedUser })
+  
 })      
 
 
