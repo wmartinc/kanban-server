25aab9c07bf0e7ca645d8a87284d759587871f23
Martin <coxmartin877@gmail.com>
2026-09-24 08:04:44 -0600

    Se modifico las dos variables de cors

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 3000
 const app = express()
 app.use(cookieParser())
 app.use(morgan('dev'))
-app.use(cors({ origin: ["https://kanbannotes.netlify.app"], credentials: true }))
+app.use(cors({ origin: ["https://kanbannotes.netlify.app", "http://localhost:5173", "*"], allowedHeaders: ["Content-Type", "Authorization"], credentials: true }))
 app.use(express.json());
 
 app.use('/api/boards', boardRoute)
